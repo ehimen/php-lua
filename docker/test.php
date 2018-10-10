b1c7e473268498eadd23c61e0f0857023e5ccd83
Andy Cook <andrewcook1990@gmail.com>
2018-10-10 17:50:55 +0100

    Added method to free up memory usage across all registered functions

--- docker/test.php
+++ docker/test.php
@@ -1,11 +1,16 @@
 <?php
 
+$lua = new Lua();
+
+
 // Adapted from https://github.com/ComaVN/php-lua-memoryleak
 
 printf("Mem at start of test (real): %d (%d)\n", memory_get_usage(), memory_get_usage(true));
 
 for ($i = 0; $i < 100; ++$i) {
-//    printf("Mem at start of loop (real): %d (%d)\n", memory_get_usage(), memory_get_usage(true));
+    printf("Mem at start of loop (real): %d (%d)\n", memory_get_usage(), memory_get_usage(true));
+
+    \Lua::clearRegisteredCallbacks();
 
     $lua = new Lua();
 
@@ -13,7 +18,9 @@ for ($i = 0; $i < 100; ++$i) {
 
     });
 
-//    printf("Mem at end of loop (real): %d (%d)\n", memory_get_usage(), memory_get_usage(true));
+    printf("Mem at end of loop (real): %d (%d)\n", memory_get_usage(), memory_get_usage(true));
 }
 $lua = null;
-printf("Mem at end of test (real): %d (%d)\n", memory_get_usage(), memory_get_usage(true));
+printf("Mem at end of test (real):   %d (%d)\n", memory_get_usage(), memory_get_usage(true)); // foo
+
+
--- lua.c
+++ lua.c
@@ -713,6 +713,19 @@ PHP_METHOD(lua, assign) {
 }
 /* }}} */
 
+/** {{{ proto proto Lua::clearRegisteredCallbacks()
+*/
+PHP_METHOD(lua, clearRegisteredCallbacks) {
+	zval *callbacks = zend_read_static_property(lua_ce, ZEND_STRL("_callbacks"), 1);
+
+ 	if (callbacks) {
+ 		zval_ptr_dtor(callbacks);
+		array_init(callbacks);
+ 	}
+}
+
+/* }}} */
+
 /** {{{ proto Lua::registerCallback(string $name, mix $value)
 */
 PHP_METHOD(lua, registerCallback) {
@@ -774,13 +787,14 @@ PHP_METHOD(lua, __construct) {
  *
  */
 zend_function_entry lua_class_methods[] = {
-	PHP_ME(lua, __construct,		NULL,  					ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
-	PHP_ME(lua, eval,          		arginfo_lua_eval,  		ZEND_ACC_PUBLIC)
-	PHP_ME(lua, include,			arginfo_lua_include, 	ZEND_ACC_PUBLIC)
-	PHP_ME(lua, call,				arginfo_lua_call,  		ZEND_ACC_PUBLIC)
-	PHP_ME(lua, assign,				arginfo_lua_assign,		ZEND_ACC_PUBLIC)
-	PHP_ME(lua, getVersion,			NULL, 					ZEND_ACC_PUBLIC|ZEND_ACC_ALLOW_STATIC)
-	PHP_ME(lua, registerCallback,	arginfo_lua_register, 	ZEND_ACC_PUBLIC)
+	PHP_ME(lua, __construct,		        NULL,  					ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
+	PHP_ME(lua, eval,          		        arginfo_lua_eval,  		ZEND_ACC_PUBLIC)
+	PHP_ME(lua, include,			        arginfo_lua_include, 	ZEND_ACC_PUBLIC)
+	PHP_ME(lua, call,				        arginfo_lua_call,  		ZEND_ACC_PUBLIC)
+	PHP_ME(lua, assign,				        arginfo_lua_assign,		ZEND_ACC_PUBLIC)
+	PHP_ME(lua, getVersion,			        NULL, 					ZEND_ACC_PUBLIC|ZEND_ACC_ALLOW_STATIC)
+	PHP_ME(lua, registerCallback,	        arginfo_lua_register, 	ZEND_ACC_PUBLIC)
+	PHP_ME(lua, clearRegisteredCallbacks,	NULL, 					ZEND_ACC_PUBLIC|ZEND_ACC_ALLOW_STATIC)
 	PHP_MALIAS(lua, __call, call, 	arginfo_lua_call,		ZEND_ACC_PUBLIC)
 	PHP_FE_END
 };
